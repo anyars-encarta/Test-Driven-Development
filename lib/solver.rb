6b0665dfadb116309008db65acf3ce09076fb9db
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-11-02 14:16:52 +0200

    fizzbuzz function

--- lib/solver.rb
+++ lib/solver.rb
@@ -11,15 +11,15 @@ class Solver
     word.reverse
   end
 
-#   def self.fizzbuzz(number)
-#     # if (number % 3).zero? && (number % 5).zero?
-#     #   'fizzbuzz'
-#     # elsif (number % 3).zero?
-#     #   'fizz'
-#     # elsif (number % 5).zero?
-#     #   'buzz'
-#     # else
-#     #   number.to_s
-#     # end
-#   end
+  def self.fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
 end
