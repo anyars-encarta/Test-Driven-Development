c7456b31de79d20f9982c0225411bd5c20342d8e
Anyars Yussif <anyarsencarta@gmail.com>
2023-11-02 10:02:33 +0000

    Create test for solver returning positive integers

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe Solver do
     it 'returns 1 for input 0' do
       expect(Solver.factorial(0)).to eq(1)
     end
+
+    it 'returns the correct factorial for positive integers' do
+      expect(Solver.factorial(5)).to eq(120)
+      expect(Solver.factorial(10)).to eq(3_628_800)
+    end
   end
 end
