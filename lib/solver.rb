b3185edcb42136ed423be08bd952313612968468
Anyars Yussif <anyarsencarta@gmail.com>
2024-01-02 14:34:23 +0000

    Update solver.rb

--- lib/solver.rb
+++ lib/solver.rb
@@ -12,14 +12,14 @@ class Solver
   end
 
   def self.fizzbuzz(number)
-    if (number % 3).zero? && (number % 5).zero?
+    if (number % 3).zero? && (number % 5).zero? 
       'fizzbuzz'
-    elsif (number % 3).zero?
+    elsif (number % 3).zero? 
       'fizz'
-    elsif (number % 5).zero?
+    elsif (number % 5).zero? 
       'buzz'
     else
-      number.to_s
+      number.to_s # else, print number
     end
   end
 end
