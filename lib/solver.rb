1a911230c2cc46e06a3f0b5495b3b015db327ceb
Anyars Yussif <anyarsencarta@gmail.com>
2024-01-02 14:35:05 +0000

    Update solver.rb

--- lib/solver.rb
+++ lib/solver.rb
@@ -14,7 +14,7 @@ class Solver
   def self.fizzbuzz(number)
     if (number % 3).zero? && (number % 5).zero? # If number mod 3 is zero, and number mod 5 is zero, print fizzbuzz
       'fizzbuzz'
-    elsif (number % 3).zero? 
+    elsif (number % 3).zero? # If number mod 3 is zero, print fizz
       'fizz'
     elsif (number % 5).zero? 
       'buzz'
